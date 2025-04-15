1dbf0fd26f342164dddf85717038cb5033e7f806
Amaral Neto <alunoamaralneto@gmail.com>
2025-04-15 16:36:46 -0300

    feat: get all users endpoint

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 import express from 'express'
 const app = express()
 const port = 3000
+// Banco de dados  fake (em memória)
+const usuarios = [
+    {id: 1, nome: 'Amaral', email: "amaralneto@gmail.com"},
+    {id: 2, nome: 'Ana', email: "ana@gmail.com"}
+    ]
+
+// Permitir ler JSON no corpo da requisição
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Bem vindo a minha API!')
 })
 
 app.post('/criarUsuario', (req, res) => {
+    const { nome, idade } = req.body
+    res.send(`Nome: ${nome} | Idade: ${idade}`)
+})
 
+/*
+Crie uma rota (endpoint) do tipo GET que
+envie uma resposta (mensagem) com todos os 
+usuários cadastrados no "Banco de dados"
+ */
+
+app.get('/usuarios', (req, res) => {
+res.send(usuarios)
 })
 
 // SERVIDOR NO AR OUVINDO REQUISIÇÕES
